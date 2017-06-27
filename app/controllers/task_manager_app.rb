0d5895b77afa436b4b7d06c17f36835775de9660
Ellen Cooper <ellencooper74@gmail.com>
2017-06-26 19:06:42 -0600

    implements updare functionality

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -21,6 +21,17 @@ class TaskManagerApp < Sinatra::Base
     erb :show
   end
 
+  get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+  set :method_override, true
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
   post '/tasks' do
    task = Task.new(params[:task])
    task.save
